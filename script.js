01b58dc5b19e7e1a3a5e6df6b9788aae2317dd00
Ugamba3 <ugodrumz3@gmail.com>
2026-09-07 22:20:09 +0100

    modified header and some new arrival

--- script.js
+++ script.js
@@ -127,13 +127,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Smooth scroll for anchor links
-  document.querySelectorAll('a[href^="#"]').forEach(a => {
-    a.addEventListener('click', e => {
-      const target = document.querySelector(a.getAttribute('href'));
-      if (target) {
-        e.preventDefault();
-        target.scrollIntoView({ behavior: 'smooth' });
-      }
-    });
-  });
+  // document.querySelectorAll('a[href^="#"]').forEach(a => {
+  //   a.addEventListener('click', e => {
+  //     const target = document.querySelector(a.getAttribute('href'));
+  //     if (target) {
+  //       e.preventDefault();
+  //       target.scrollIntoView({ behavior: 'smooth' });
+  //     }
+  //   });
+  // });
 });
